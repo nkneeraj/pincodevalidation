1a3e401a7464abafe7c5d0b6c0947b7460bb0692
nkneeraj <nkneerj641@gmail.com>
2020-09-28 22:52:17 +0530

    [NK]Added UC1: Validate 6 digit pincode

--- src/com/cg/pinCode/PinCodeValidation.java
+++ src/com/cg/pinCode/PinCodeValidation.java
@@ -1,9 +1,23 @@
 package com.cg.pinCode;
 
+import java.util.*;
+import java.util.regex.*;
+
 public class PinCodeValidation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Welcome to pincode validation");
+		Pattern pincode = Pattern.compile("^[0-9]{6}$");
+		System.out.println("Enter the pincode");
+		String match = sc.nextLine();
+		Matcher matcher = pincode.matcher(match);
+		boolean found = matcher.find();
+		if (found)
+			System.out.println("Match Found");
+		else
+			System.out.println("Match not found");
 
 	}
 
